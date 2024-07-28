8a720d8aa2cc5feefda4ee78d28d562a115ebc04
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-29 02:21:18 +0300

    create extentions

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:tech_haven/router/app_router.dart';
+import 'package:tech_haven/utils/constants/routes.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Tech Haven',
       debugShowCheckedModeBanner: false,
+      initialRoute: RouteManager.initialRoute,
+      onGenerateRoute: AppRouter.onGenerateRoute,
     );
   }
 }
