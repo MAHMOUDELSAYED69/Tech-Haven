c0e6bd808b8532ede2088f3c492dee75cae1edc6
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-30 01:22:48 +0300

    create BoxShadowManager class, GradientManager class

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -2,9 +2,38 @@ import 'package:flutter/material.dart';
 
 abstract class ColorManager {
   const ColorManager._();
+
   static const Color blue = Color(0xff0062BD);
   static const Color white = Color(0xffFFFFFF);
   static const Color black = Color(0xff000000);
   static const Color grey = Color(0xff464646);
-  static const Color transparent =Colors.transparent;
+  static const Color error = Color(0xffF85555);
+  static const Color correct = Color(0xff25BD5D);
+  static const Color transparent = Colors.transparent;
+
+}
+
+abstract class BoxShadowManager {
+  const BoxShadowManager._();
+
+  static List<BoxShadow>? boxShadow = [
+    BoxShadow(
+      color: ColorManager.black.withOpacity(0.25),
+      blurRadius: 20,
+      spreadRadius: 5.0,
+      offset: const Offset(2.0, 5.0),
+    ),
+  ];
+}
+abstract class GradientManager {
+  const GradientManager._();
+
+  static Gradient buttonGradient = LinearGradient(
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+      colors: [
+        ColorManager.blue,
+        ColorManager.blue.withOpacity(0.6),
+        ColorManager.blue.withOpacity(0.37),
+      ]);
 }
