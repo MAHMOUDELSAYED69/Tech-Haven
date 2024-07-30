91a73b5cf816f7cf6cba586c51b89cdab464b82e
MahmoudElSayed96 <gcfjxvkj@gmail.com>
2024-07-30 06:30:52 +0300

    create OTPField

---
+++ lib/view/widgets/otp_field.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:tech_haven/utils/constants/colors.dart';
+import 'package:tech_haven/utils/extentions/extentions.dart';
+
+class OTPField extends StatelessWidget {
+  const OTPField({
+    super.key,
+    this.onSaved,
+  });
+
+  final void Function(String?)? onSaved;
+
+  @override
+  Widget build(BuildContext context) {
+    const errorBorder =OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(16)),
+      borderSide: BorderSide(
+          color: ColorManager.error,
+          width: 1.5
+      ),
+    );
+    const border = OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        borderSide: BorderSide.none
+    );
+    return Container(
+      height: 64,
+      width: 60,
+      decoration: BoxDecoration(
+        boxShadow: BoxShadowManager.boxShadow,
+      ),
+      child: TextFormField(
+        autofocus: true,
+        onChanged: (value) {
+          if (value.length == 1) {
+            FocusScope.of(context).nextFocus();
+          }
+          if (value.isEmpty) {
+            FocusScope.of(context).previousFocus();
+          }
+        },
+        validator: (value) {
+          if (value!.isEmpty) {
+            return '';
+          }
+          return null;
+        },
+        onSaved: onSaved,
+        keyboardType: TextInputType.number,
+        maxLength: 1,
+        decoration: const InputDecoration(
+          counterText: '',
+          border: border,
+          enabledBorder: border,
+          focusedBorder: border,
+          errorBorder: errorBorder,
+          focusedErrorBorder:errorBorder,
+          fillColor: ColorManager.white,
+          filled: true,
+        ),
+        textAlign: TextAlign.center,
+        style: context.textTheme.displayLarge,
+      ),
+    );
+  }
+}
